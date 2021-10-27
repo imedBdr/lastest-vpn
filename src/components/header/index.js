e252c999cce66753d74cad858e1030242322e714
Imed <dak007.ib@gmail.com>
2021-10-27 15:49:44 +0100

    complete mobile view version

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 import logo from "../../assets/images/logo.svg"
 import "./header.scss";
 
-const Header = (props) =>{
+const Header = () =>{
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleToogleMenu = () =>{
+        setIsOpen(!isOpen)
+    }
     return(
         <div className={"Header"}>
         <div className={"logo"}>
             <img src={logo} alt={"logo"}></img>
         </div>
-        <ul className={"navbar"}>
-            <li><Link to={"/"}>About</Link></li>
-            <li><Link to={"/"}>Features</Link></li>
-            <li><Link to={"/"}>Pricing</Link></li>
-            <li><Link to={"/"}>Testimontals</Link></li>
-            <li><Link to={"/"}>Help</Link></li>
-        </ul>
-        <div className={"auth"}>
-            <Link to ={"/"} className={""}>Sign In</Link>
-            <Link className={"btn btn-outlined btn-md"} to={"/"}>Sign Up</Link>
-        </div>        
+        <div className={`row  ${ isOpen ? 'open' : '' }`}>
+            <ul className={`navbar`}>
+                <li><Link to={"/"}>About</Link></li>
+                <li><Link to={"/"}>Features</Link></li>
+                <li><Link to={"/"}>Pricing</Link></li>
+                <li><Link to={"/"}>Testimontals</Link></li>
+                <li><Link to={"/"}>Help</Link></li>
+            </ul>
+            <div className={"auth"}>
+                <Link to ={"/"} className={""}>Sign In</Link>
+                <Link className={"btn btn-outlined btn-md"} to={"/"}>Sign Up</Link>
+            </div>
+        </div>   
+        <div id={'toogleMenu'} onClick={handleToogleMenu}>
+            <i className={'material-icons'}>menu</i>    
+        </div>   
     </div>)
 }
 
